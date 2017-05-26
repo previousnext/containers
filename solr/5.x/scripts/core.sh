3599a361b3144a0afb7ff728ed3afa87ad65fee7
Nick Schuch <nick@myschuch.com>
2017-05-26 14:43:29 +1000

    Fixes core provisioning in Solr .5x

--- solr/5.x/scripts/core.sh
+++ solr/5.x/scripts/core.sh
@@ -3,6 +3,15 @@
 # Name:        core.sh
 # Description: Sets up the Solr core. Default SOLR_CORE set in Dockerfile.
 
+FILE=/opt/solr/provisioned
+
+if [ -f "${FILE}" ]; then
+  echo "The core was already provisioned"
+  return
+fi
+
 start-local-solr && \
   /opt/solr/bin/solr create -c $SOLR_CORE -d /opt/search_api && \
   stop-local-solr
+
+echo $(date) > $FILE
