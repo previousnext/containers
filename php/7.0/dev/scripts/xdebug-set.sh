a0934288ec371d81e3ba851882207984813b752a
Nick Schuch <nick@myschuch.com>
2017-08-24 12:48:42 +1000

    Fix xdebug sed script

--- php/7.0/dev/scripts/xdebug-set.sh
+++ php/7.0/dev/scripts/xdebug-set.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-XDEBUG_SO=$(ls /usr/local/lib/php/extensions/*/xdebug.so | head -n1)
+XDEBUG_SO=$(ls /usr/local/lib/php/extensions/*/xdebug.so | head -n1 | sed 's/\//\\\//g')
 
-sed -i -e "s/XDEBUG_SO/${XDEBUG_SO}/g" $1
+sed -i -e "s/XDEBUG_SO/'$XDEBUG_SO'/g" $1
